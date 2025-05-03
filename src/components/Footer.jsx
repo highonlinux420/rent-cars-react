fc2c6bcc72b97511b08363516d6e10cdc003b1a6
highonlinux420 <adelbourouis9@gmail.com>
2025-05-03 17:30:42 +0100

    Changed info to Info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Info from "./info";
+import Info from "./Info";
 import play from "../assets/play.png";
 import apple from "../assets/apple.svg";
 import {Link} from "react-router-dom";
